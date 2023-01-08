b3e38c71d4e346925296f67329495a79f8464df3
Umesh-Phendarkar <umesh.phendarkar@gmail.com>
2023-01-08 11:52:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "remote" {
     organization = "Skills01"
     workspaces {
-      name = "terraform-jan2023-New"
+      name = "Workspace-new-cli"
     }
   }
 
@@ -18,9 +18,9 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_instance" "myFirstinstance" {
-  ami = "ami-08df646e18b182346"
+  ami           = "ami-08df646e18b182346"
   instance_type = "t2.small"
-tags = {
-        Name = "my-ec2"
-}
+  tags = {
+    Name = "my-ec2"
+  }
 }
